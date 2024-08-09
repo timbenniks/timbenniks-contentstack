8e60b3c9f6f833749a5520959ecde3718ddda16b
Tim Benniks <tbenniks@gmail.com>
2024-08-09 09:25:06 +0200

    CSP fixed?

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,7 +19,7 @@ export default defineNuxtConfig({
     "@nuxtjs/tailwindcss",
     "@nuxt/fonts",
     "nuxt-jsonld",
-    //"nuxt-security"
+    "nuxt-security"
   ],
 
   devServer: {
@@ -40,50 +40,32 @@ export default defineNuxtConfig({
     },
   },
 
-  // security: {
-  //   headers: {
-  //     contentSecurityPolicy: {
-  //       'style-src': [
-  //         "'self'",
-  //         "'report-sample'",
-  //         "'unsafe-inline'",
-  //         "*.csnonprod.com",
-  //         "*.contentstack.com",
-  //         "*.salesforceliveagent.com",
-  //         "https://*.salesforce-sites.com",
-  //         "https://rawgithub.com",
-  //         "https://*.bootstrapcdn.com",
-  //         "https://cdnjs.cloudflare.com",
-  //         "https://fonts.googleapis.com",
-  //         "https://service.force.com",
-  //         "https://liveagentcontentstack.secure.force.com",
-  //         "https://fast.appcues.com",
-  //         "https://frames-commandbar-prod.commandbar.com",
-  //         "https://cdn.commandbar.com"
-  //       ],
-  //       'connect-src': [
-  //         "'self'",
-  //         "*.csnonprod.com",
-  //         "*.contentstack.com",
-  //         "*.salesforce-sites.com",
-  //         "https://cdn-personalization.contentstack.com",
-  //         "https://cdn.contentstack.io",
-  //         "https://api.appcues.net",
-  //         "wss://api.appcues.net",
-  //         "https://liveagentcontentstack.secure.force.com",
-  //         "https://api-iam.intercom.io",
-  //         "wss://nexus-websocket-a.intercom.io",
-  //         "wss://ws-mt1.pusher.com",
-  //         "https://widget.usersnap.com",
-  //         "https://api.commandbar.com",
-  //         "https://t.commandbar.com",
-  //         "https://s3.us-west-2.amazonaws.com",
-  //         "https://*.browser-intake-datadoghq.eu",
-  //         "wss://ws-eu.pusher.com" // Add this line
-  //       ],
-  //     },
-  //   },
-  // },
+  security: {
+    headers: {
+      contentSecurityPolicy: {
+        'connect-src': [
+          "'self'",
+          '*.csnonprod.com',
+          '*.contentstack.com',
+          '*.salesforce-sites.com',
+          'https://cdn-personalization.contentstack.com',
+          'https://cdn.contentstack.io',
+          'https://api.appcues.net',
+          'wss://api.appcues.net',
+          'https://liveagentcontentstack.secure.force.com',
+          'https://api-iam.intercom.io',
+          'wss://nexus-websocket-a.intercom.io',
+          'wss://ws-mt1.pusher.com',
+          'https://widget.usersnap.com',
+          'https://api.commandbar.com',
+          'https://t.commandbar.com',
+          'https://s3.us-west-2.amazonaws.com',
+          'https://*.browser-intake-datadoghq.eu',
+          'https://timbenniks-contentstack.vercel.app' // Added URL
+        ]
+      },
+    },
+  },
   features: {
     noScripts: false,
     inlineStyles: true
