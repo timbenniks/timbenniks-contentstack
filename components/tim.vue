6484c7683f39c2fc0abcdc7e56ccd006988c71af
Tim Benniks <tbenniks@gmail.com>
2024-09-11 16:10:44 +0200

    launch fixes

---
+++ components/tim.vue
@@ -0,0 +1,3 @@
+<template>
+  <img src="/tim.svg" alt="Logo Tim Benniks" width="43" height="61" />
+</template>
---
+++ components/twoColumns.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="w-full mb-8">
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-12">
+      <div><slot name="sidea" /></div>
+      <div><slot name="sideb" /></div>
+    </div>
+  </div>
+</template>
