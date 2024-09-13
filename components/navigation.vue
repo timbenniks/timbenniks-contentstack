b14217e3b73bbeb5481cf16d8794d133783f9ceb
Tim Benniks <tbenniks@gmail.com>
2024-09-13 17:30:16 +0200

    status updates

--- components/navigation.vue
+++ components/navigation.vue
@@ -5,11 +5,11 @@ const { $preview } = useNuxtApp();
 
 <template>
   <globalHeader :navigation="navigation" searchurl="/search">
-    <div
-      class="sticky top-0 p-2 bg-[#256ad1] uppercase text-sm"
-      v-if="$preview"
-    >
-      ⚡️ Preview mode: on
+    <div class="sticky top-0 p-1 bg-[#256ad1]" v-if="$preview">
+      <span class="uppercase text-sm pr-2">⚡️ Preview mode: on</span>
+      <span class="text-xs text-slate-300"
+        >Preview sdk v1.4.4. Region: EU. Environment: development</span
+      >
     </div>
   </globalHeader>
 </template>
