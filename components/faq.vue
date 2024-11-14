63170527438ad2f07a4ff8a043670d226ebde10d
Tim Benniks <tbenniks@gmail.com>
2024-11-14 12:21:31 +0100

    in edit mode show different faq

--- components/faq.vue
+++ components/faq.vue
@@ -8,45 +8,7 @@ const { livePreviewEnabled } = useNuxtApp().$contentstack as {
 
 <template>
   <div itemscope itemtype="https://schema.org/FAQPage">
-    <template v-if="!big">
-      <h4 v-if="title" class="title inline-block small mb-4">
-        {{ title }}
-      </h4>
-      <ul class="bg-[#0e1029] pt-4 pb-1 px-4">
-        <li
-          v-for="faq in faqs"
-          :key="faq.question"
-          v-bind="faq.wrapperCslp && faq.wrapperCslp"
-          itemscope
-          itemprop="mainEntity"
-          itemtype="https://schema.org/Question"
-        >
-          <details class="mb-4" :open="livePreviewEnabled">
-            <summary
-              class="bg-[#1b1d39] hover:opacity-90 py-2 px-4 cursor-pointer font-bold"
-              itemprop="name"
-              v-bind="faq.innerCslp && faq.innerCslp.question"
-            >
-              {{ faq.question }}
-            </summary>
-
-            <div
-              itemscope
-              itemprop="acceptedAnswer"
-              itemtype="https://schema.org/Answer"
-              class="text-sm my-4 px-4 text-slate-400"
-            >
-              <div
-                itemprop="text"
-                v-html="faq.answer"
-                v-bind="faq.innerCslp && faq.innerCslp.answer"
-              />
-            </div>
-          </details>
-        </li>
-      </ul>
-    </template>
-    <template v-else>
+    <template v-if="big || livePreviewEnabled">
       <div class="pb-12 px-8">
         <h3 class="title inline-block small mb-8">
           {{ title }}
@@ -82,6 +44,44 @@ const { livePreviewEnabled } = useNuxtApp().$contentstack as {
           </li>
         </ul>
       </div>
+    </template>
+    <template v-else>
+      <h4 v-if="title" class="title inline-block small mb-4">
+        {{ title }}
+      </h4>
+      <ul class="bg-[#0e1029] pt-4 pb-1 px-4">
+        <li
+          v-for="faq in faqs"
+          :key="faq.question"
+          v-bind="faq.wrapperCslp && faq.wrapperCslp"
+          itemscope
+          itemprop="mainEntity"
+          itemtype="https://schema.org/Question"
+        >
+          <details class="mb-4">
+            <summary
+              class="bg-[#1b1d39] hover:opacity-90 py-2 px-4 cursor-pointer font-bold"
+              itemprop="name"
+              v-bind="faq.innerCslp && faq.innerCslp.question"
+            >
+              {{ faq.question }}
+            </summary>
+
+            <div
+              itemscope
+              itemprop="acceptedAnswer"
+              itemtype="https://schema.org/Answer"
+              class="text-sm my-4 px-4 text-slate-400"
+            >
+              <div
+                itemprop="text"
+                v-html="faq.answer"
+                v-bind="faq.innerCslp && faq.innerCslp.answer"
+              />
+            </div>
+          </details>
+        </li>
+      </ul>
     </template>
   </div>
 </template>
