5ea3aa849d9d91ec0b5282147b984354edbcf323
Tim Benniks <tbenniks@gmail.com>
2025-01-28 16:49:02 +0100

    add contentstackEdgeApi for entityPush personalization

--- app.vue
+++ app.vue
@@ -70,6 +70,7 @@ useHead({
           entityPush: {
             disabled: false,
             personalizeProjectId: '673de37a605735e17555d300',
+            contentstackEdgeApi: 'https://eu-personalize-edge.contentstack.com'
           }
         }
       });
